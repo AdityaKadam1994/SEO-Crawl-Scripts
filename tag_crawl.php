60e2261b590c8a08ac21dabc16476e52df6d8450
Aditya <addy.7kad@gmail.com>
2020-06-13 13:39:33 +0530

    added UI for content crawling combined seo crawling scripts

--- tag_crawl.php
+++ tag_crawl.php
@@ -1,26 +1,81 @@
 <?php 
 	
-function crawlPage($url) { 
+function crawlPage($url,$arrr) { 
 
 	$dom = new DOMDocument; 
 	
 	// Loading HTML content in $dom 
-	@$dom->loadHTMLFile($url); 
-	
-	
-	$node = $dom -> getElementsByTagName('a'); 
-	
-	
-	// Extracting attribute from each object 
-	foreach ($node as $element) { 
-	
-		echo $element -> nodeValue;
+	foreach ($url as $linkname) {
+		echo $linkname;
 		echo '<br>';
 
-	} 
+		@$dom->loadHTMLFile($linkname); 
+		foreach($arrr as $tagname ){
+			$node = $dom -> getElementsByTagName($tagname); 
+			echo '<b>'.$tagname.'</b>';
+			echo '<br>';
+			
+			// Extracting attribute from each object 
+			if ($tagname == 'meta') {
+	
+				foreach ($node as $element) { 
+					if ($element -> getAttribute('name')){
+						echo "<b>".$element -> getAttribute('name')."</b>".":".$element -> getAttribute('content'); 
+						echo "<br>";
+					}else {
 
-} 
+						echo "<b>".$element -> getAttribute('property')."</b>".":".$element -> getAttribute('content'); 
+						echo '<br>';
+					}
+					
+				}
 
-crawlPage("htdocs/web.html"); 
+			}
+			elseif($tagname == 'a') {
+				foreach ($node as $element) { 
+					echo $element -> getAttribute('href'); 
+					echo '<br>';
+				}
+			}
+			elseif($tagname == 'img') {
+				echo '<br>';
+				foreach ($node as $element) { 
+					$src = $element -> getAttribute('src'); 
+					$alt = $element -> getAttribute('alt'); 
+					
+					echo 'src='.$src.' alt='.$alt.'<br>';
+				}
+			}
+			elseif($tagname == 'link') {
+				foreach ($node as $element) { 
+					$atr = $element -> getAttribute('rel'); 
+					if ($atr == 'canonical') {
+						echo '<b>Canonical:</b> '.$element -> getAttribute('href').'<br>'; 
+						}
+				}
+			}
+			else {
+				foreach ($node as $element) { 
+						// echo $element -> nodeValue;
+						echo $element -> nodeValue;
+						// echo $element -> tagName;
+						echo '<br>';
+					
+				}
+				echo '<br>';
+			}
+			 
+		}
+
+		echo '<br>';
+		echo '>>>>>>>>>>>>>>>>>>>>END>>>>>>>>>>>>>>>>>>>>>>>>>>><br>';
+	}
+	
+}
+
+crawlPage(["https://www.itimf.com/locate_advisor/east.aspx",
+"https://www.itimf.com/locate_advisor/west.aspx",
+"https://www.itimf.com/for-investors/faqs/ISIP"],['h1','h2','h3','h4','h5','h6','a','img','meta','link']); 
+
 
 ?> 
